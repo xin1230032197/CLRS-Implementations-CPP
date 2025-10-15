0b04167f358659304313afb0fbc2f844fdf2577a
MIKU_HONG <3129753672@qq.com>
2025-10-15 21:07:39 +0800

    选择新的架构所以先把代码都删掉了

--- clrscpp/clrscpp.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-int main() {
-	int a;
-	char b;
-	cout << "text" << endl;
-	cout << "this is most text of the git of it";
-	//ok this is sound good and i will begin to do it
-	}
